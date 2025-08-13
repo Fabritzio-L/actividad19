f1b80b759988d742748c9139682ce10265265b05
fabri <fabritzioalejandrol@gmail.com>
2025-08-13 17:39:46 -0600

    Clases de GalletaChispas y de relleno

--- actividad19.py
+++ actividad19.py
@@ -11,3 +11,18 @@ class Galleta:
         self.peso=peso
     def mostrar_info(self):
         return f"Galleta: {self.nombre}|Precio: {self.precio}|Peso: {self.peso}"
+class GalletaChispas(Galleta):
+    def __init__(self,nombre,precio,peso,cantidad_chispas):
+        super.__init__(nombre,precio,peso)
+        self.cantidad_chispas=cantidad_chispas
+        if cantidad_chispas <0:
+            print("La cantidad de chispas no puede ser negativa")
+    def mostrar_info(self):
+        return super().mostrar_info()+ f"|Chispas: {self.cantidad_chispas}"
+class Relleno:
+    def __init__(self,sabor_relleno):
+        self.sabor_relleno=sabor_relleno
+        if not sabor_relleno:
+            print("El sabor del relleno no puede estar vacio")
+    def describir_relleno(self):
+        return f"Relleno sabor a {self.sabor_relleno}"
